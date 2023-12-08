8c3bec68d332779744b7d89c244792c4be15ae46
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-12-08 19:39:40 -0300

    att P004 parte 2

--- semana04/P004/script.js
+++ semana04/P004/script.js
@@ -3,6 +3,41 @@
 // box-shadow 
 // transform
 
-const botao = document.querySelector('button');
+let fortaleza = document.getElementById("fortaleza");
+let salvador = document.getElementById("salvador");
+let campinas = document.getElementById("campinas");
 
-botao.addEventListener('')
+let butao_fortaleza = fortaleza.querySelector("button");
+let butao_salvador = salvador.querySelector("button");
+let butao_campinas = campinas.querySelector("button");
+
+butao_fortaleza.addEventListener("click", function (){
+    getInfo(fortaleza);
+});
+
+butao_salvador.addEventListener("click", function (){
+    getInfo(salvador);
+});
+butao_campinas.addEventListener("click", function (){
+    getInfo(campinas);
+});
+
+
+function getInfo(local){
+    let itens = local.querySelectorAll("li");
+    let taxas = local.getElementsByClassName("taxes")[0].textContent.trim();
+    let preco = local.getElementsByClassName("value")[0].textContent.trim();
+    let parcela = local.getElementsByClassName("portion")[0].textContent.trim();
+    let titulo = local.querySelector("h2").textContent.trim();
+    
+    let json = {"local": titulo, "itens": [], "preco": preco, "parcela": parcela, "taxas": taxas};
+    for (let i = 0; i < itens.length; i++) {
+        let item = itens[i].textContent.trim();
+        json.itens.push(item);
+    }    
+
+
+
+    console.log(json);
+
+};
