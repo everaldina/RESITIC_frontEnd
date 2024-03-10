9bc703a6fa9d527d51b3607b6aa693864c69e1b7
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-03-09 22:23:36 -0300

    upload firebase

--- semana13/P013/src/app/appointment-data.service.ts
+++ semana13/P013/src/app/appointment-data.service.ts
@@ -10,7 +10,7 @@ import { retry, catchError } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class AppointmentDataService {
-  endpoint = 'https://p013-1bbb3-default-rtdb.firebaseio.com';
+  endpoint = 'https://p013-c3262-default-rtdb.firebaseio.com';
   constructor(private http: HttpClient) {}
 
   httpOptions = {
