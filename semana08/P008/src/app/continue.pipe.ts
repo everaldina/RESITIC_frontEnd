53bf07e57d61b9771b3ba282ffddcddb459b935f
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-03-11 21:53:02 -0300

    att continue pipe P008

--- semana08/P008/src/app/continue.pipe.ts
+++ semana08/P008/src/app/continue.pipe.ts
@@ -7,7 +7,17 @@ export class ContinuePipe implements PipeTransform {
 
   transform(value: unknown): unknown {
     if (value) {
-      return value + '...';
+      if (typeof value !== 'string') {
+        return value;
+      }
+
+      const isUpperCase = (str:string) => str === str.toUpperCase();
+      let transform = value as string;
+      transform = transform.trim();
+      if (isUpperCase(transform[0]))
+        return value + '...';
+      else
+        return '... ' + value + '...';
     }
     return value;
   }
